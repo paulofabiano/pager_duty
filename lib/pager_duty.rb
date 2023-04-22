613a46b35f92f89456a22a2ccb8f6057728064c3
Paulo Langer <paulo.langer@xing.com>
2023-04-22 21:43:38 +0100

    refactor: Using composition instead inheritance

--- lib/pager_duty.rb
+++ lib/pager_duty.rb
@@ -6,10 +6,18 @@ end
 
 module PagerDuty
   class Client
-    attr_reader :users
+    include HTTParty
+    include UserResource
 
     def initialize(auth_token)
-      @users = PagerDuty::Resources::User.new(auth_token)
+      self.class.base_uri 'https://api.pagerduty.com'
+      self.class.default_options.merge!(
+        headers: {
+          'Accept' => 'application/vnd.pagerduty+json;version=2',
+          'Authorization' => "Token token=#{auth_token}",
+          'Content-Type' => 'application/json'
+        }
+      )
     end
   end
 end
--- lib/pager_duty/resources/user.rb
+++ lib/pager_duty/resources/user.rb
@@ -1,9 +1,7 @@
 module PagerDuty
-  module Resources
-    class User < Base
-      def list_users
-        self.class.get('/users')
-      end
+  module UserResource
+    def list_users
+      self.class.get('/users')
     end
   end
 end
